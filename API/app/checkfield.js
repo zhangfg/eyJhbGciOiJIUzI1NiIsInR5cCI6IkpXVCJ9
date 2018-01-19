55321f9e1d3b732930423e8aec362d34e17f176b
Kris <andy_liu2012@qq.com>
2018-01-19 15:27:29 +0800

    Update checkfield.js
    修改了flex生成po时对字段的检查条件

--- API/app/checkfield.js
+++ API/app/checkfield.js
@@ -20,77 +20,218 @@ var hfc = require('fabric-client');
 
 
 
- function check(fcn,args)
+exports.checkField = function (fcn,args)
 {
-    if(fcn == 'crCPurchaseOrderInfo')
+    if(fcn === 'crCPurchaseOrderInfo')
     {
-        switch(args.TRANSDOC){
-            case 'GR':
-            if(typeof args.CPONO == string && args.CPONO.length<= 35){
-                if(typeof args.ODMGRInfos.GRNO == string&&typeof args.ODMGRInfos.PARTNUM==string&&args.ODMGRInfos.PARTNUM.length<=15&&typeof args.ODMGRInfos.GRQTY==number){
+        var reg = /^[a-zA-Z0-9]+$/;
+        for(var i=0;i<args.length;i++){
+            switch(args[i].TRANSDOC){
+                case 'GR':
+                if(typeof args[i].CPONO ===string && args[i].CPONO.length <= 35){
                     return '';
                 }else{
-                    return 'check error';
+                    return 'CPONO check error';
                 }
-            }
-            break;
-            case 'PY':
-            if(typeof args.CPONO == string && args.CPONO.length<= 35){
-                if(typeof args.ODMPayments.FlexInvoiceNO == string && typeof args.ODMPayments.GRNO ==string && typeof args.ODMPayments.BILLINGNO == string 
-                    && args.ODMPayments.BILLINGNO.length==10&&typeof args.ODMPayments.INVOICESTATUS==string && args.ODMPayments.INVOICESTATUS.length==1
-                    &&args.ODMPayments.INVOICESTATUS=='A' ||args.ODMPayments.INVOICESTATUS=='P' &&checkDate2(args.ODMPayments.PAYMENTDATE) && args.ODMPayments.PAYMENTDATE.length==8){
+                if(typeof args[i].GRNO ===string){
                     return '';
                 }else{
-                    return 'check error';
+                    return 'GRNO check error';
                 }
-            }
-            break;
-            case 'LP':
-            if(typeof args.CPONO == string && args.CPONO.length<= 35){
-                if(typeof args.LOIMaterials.RefNo == string && typeof args.LOIMaterials.PullType == string && args.LOIMaterials.PullType.length==3
-                    &&args.LOIMaterials.PullType == 'LOI'&& typeof args.LOIMaterials.Week==string && args.LOIMaterials.Week.length ==8 
-                    &&typeof args.LOIMaterials.NotesToReceiver == string &&typeof args.LOIMaterials.NotesToReceiver==string&&args.LOIMaterials.NotesToReceiver.length<=18
-                    &&typeof args.LOIMaterials.Quantity == number&& CheckDate2(args.LOIMaterials.DlveryDate)&&args.LOIMaterials.DlveryDate.length==8
-                    &&CheckDate2(args.LOIMaterials.RequestedDate)&&args.LOIMaterials.RequestedDat==8&&typeof args.LOIMaterials.ShipmentInstruction==string){
+                if(typeof args[i].PARTNUM <= 18 && reg.test(args[i].PARTNUM)){
                     return '';
                 }else{
-                    return 'check error';
+                    return 'PARTNUM check error';
                 }
-            }
-            break;
-            case 'LG':
-            if(typeof args.GRNO == string ){
-                if(typeof args.PN == string && args.PN.length==18&&typeof args.Qty == number&&CheckDate2(args.GRDate) 
-                && args.GRDate.length == 8){
+                if(typeof args[i].GRQTY===number){
                     return '';
                 }else{
-                    return 'check error';
+                    return 'GRQTY check error';
                 }
+                break;
+
+                case 'PY':
+                if(typeof args[i].CPONO === string && args[i].CPONO.length<= 35){
+                    return '';
+                }else{
+                    return 'CPONO check error';
+                }
+                for(var j=0;j<args[i].ODMPayments.length;j++){
+                    if(typeof args[i].ODMPayments[j].FlexInvoiceNO === string){
+                        return '';
+                    }else{
+                        return 'FlexInvoiceNO check error';
+                    }
+                    if(typeof args[i].ODMPayments[j].GRNO ===string){
+                        return '';
+                    }else{
+                        return 'GRNO check error';
+                    }
+                    if(typeof args[i].ODMPayments[j].BILLINGNO === string 
+                        && args[i].ODMPayments[j].BILLINGNO.length==10){
+                            return '';
+                        }else{
+                            return 'BILLINGNO check error';
+                        }
+                    if(typeof args[i].ODMPayments[j].INVOICESTATUS==string && args[i].ODMPayments[j].INVOICESTATUS.length==1
+                        &&args[i].ODMPayments[j].INVOICESTATUS=='A' ||args[i].ODMPayments[j].INVOICESTATUS=='P'){
+                            return '';
+                        }else{
+                            return 'INVOICESTATUS check error';
+                        }
+                     if(checkDate2(args[i].ODMPayments[j].PAYMENTDATE) && args[i].ODMPayments[j].PAYMENTDATE.length==8){
+                         return '';
+                     }else{
+                         return 'PAYMENTDATE check error';
+                     }    
+                }
+                break;
+                
+                case 'LP':
+                if(typeof args[i].CPONO == string && args[i].CPONO.length<= 35){
+                    for(var j =0;j<args[i].LOIMaterials;j++){
+                        if(typeof args[i].LOIMaterials[j].RefNo === string){
+                            return '';
+                        }else{
+                            return 'CPONO check error';
+                        }
+                        if(typeof args[i].LOIMaterials[j].PullType === string && args[i].LOIMaterials[j].PullType.length===3
+                            &&args[i].LOIMaterials[j].PullType === 'LOI'){
+                                return '';
+                            }else{
+                                return 'PullType check error';
+                            }
+                         if(typeof args[i].LOIMaterials[j].Week===string && args[i].LOIMaterials[j].Week.length ===8){
+                            return '';
+                         }else{
+                            return 'Week check error';
+                         }
+                         if(typeof args[i].LOIMaterials[j].NotesToReceiver===string&&args[i].LOIMaterials[j].NotesToReceiver.length<=18){
+                             return '';
+                         }else{
+                             return 'NotesToReceiver check error';
+                         }  
+                         if(typeof args[i].LOIMaterials[j].Quantity === number){
+                             return '';
+                         } else{
+                             return 'Quantity check error';
+                         }
+                         if(CheckDate2(args[i].LOIMaterials[j].DlveryDate)&&args[i].LOIMaterials[j].DlveryDate.length===8){
+                            return '';
+                         }else{
+                             return 'DlveryDate check error';
+                         }
+                         if(CheckDate2(args[i].LOIMaterials[j].RequestedDate)&&args[i].LOIMaterials[j].RequestedDat===8){
+                             return '';
+                         }else{
+                             return 'RequestedDate check error';
+                         }
+                         if(args[i].LOIMaterials[j].ShipmentInstruction===string){
+                             return '';
+                         }else{
+                             return 'ShipmentInstruction check error';
+                         }
+                    }
+
+                }
+                break;
+
+                case 'LG':
+                if(typeof args[i].GRNO === string){
+                    return '';
+                }else{
+                    return 'GRNO check error';
+                }
+                if(typeof args[i].PN === string && args[i].PN.length==18){
+                    return '';
+                }else{
+                    return 'PN check error';
+                }
+                if(typeof args[i].Qty === number){
+                    return '';
+                }else{
+                    return 'Qty check error';
+                }
+                if(CheckDate2(args[i].GRDate)&& args[i].GRDate.length === 8){
+                    return '';
+                }else{
+                    return 'GRDate check error';
+                }
+                break; 
+                case 'SP':
+                if(typeof args[i].CPONO === string && args[i].CPONO.length<= 35){
+                    return '';
+                }else{
+                    return 'CPONO check error';
+                }
+                for(var j =0;j<args[i].SOIPulls;j++){
+                    if(typeof args[i].SOIPulls[j].PullType === string && args[i].SOIPulls[j].PullType.length===3 && args[i].SOIPulls[j].PullType==='LOI'){
+                        return '';
+                    }else{
+                        return 'PullType check error';
+                    }
+                    if(typeof args[i].SOIPulls[j].Week === string && args[i].SOIPulls[j].Week.length===8){
+                        return '';
+                    }else{
+                        return 'Week check error';
+                    }
+                    if(typeof CheckDate2(args[i].SOIPulls[j].PullDate)  
+                    && args[i].SOIPulls[j].PullDate.length ===8){
+                        return '';
+                    }else{
+                        return 'PullDate check error';
+                    }
+                    if(typeof args[i].SOIPulls[j].PN === string && args[i].SOIPulls[j].PN.length<=18){
+                        return '';
+                    }else{
+                        return 'PN check error';
+                    }
+                    if(typeof args[i].SOIPulls[j].Qty === number){
+                        return '';
+                    }else{
+                        return 'Qty check error';
+                    }
+                    if(typeof args[i].SOIPulls[j].RefNo ===string && args[i].SOIPulls[j].RefNo.length==10 ){
+                        return '';
+                    }else{
+                        return 'RefNo check error';
+                    }
+                   
+                }
+               
+                break;
+                case 'SI':
+                if(typeof args[i].PN === string && args[i].PN.length<= 18)
+                {
+                    return '';
+                }else{
+                    return 'PN check error';
+                }
+                if(typeof args[i].PartDesc === string){
+                    return '';
+                }else{
+                    return 'PartDesc check error';
+                }
+                if(typeof args[i].InventoryType === string
+                    &&args[i].InventoryType.length ===3 && args[i].InventoryType === 'SOI'){
+                        return '';
+                    }else{
+                        return 'InventoryType check error';
+                    }
+                if(typeof args[i].Qty === number){
+                    return '';
+                }else{
+                    return 'Qty check error';
+                }
+                if(typeof args[i].SupplierName === string){
+                    return '';
+                }else{
+                    return 'SupplierName check error';
+                }
+                break; 
 
             }
-            break;
-            case 'SP':
-            if(typeof args.CPONO == string && args.CPONO.length<= 35){
-                if(typeof args.SOIPulls.PullType == string && args.SOIPulls.PullType.length==3 && args.SOIPulls.PullType=='LOI'
-                &&typeof args.SOIPulls.Week == string && args.SOIPulls.Week.length==8&&typeof CheckDate2(args.SOIPulls.PullDate)  
-                && args.SOIPulls.PullDate.length ==8 &&typeof args.SOIPulls.PN == string && args.SOIPulls.PN.length<=18
-                &&typeof args.SOIPulls.Qty == number&&typeof args.SOIPulls.RefNo ==string && args.SOIPulls.RefNo.length==10 ){
-                    return '';
-                }else{
-                    return 'check error';
-                }
-            }
-            break;
-            case 'SI':
-            if(typeof args.PN == string && args.PN.length<= 18 &&typeof args.PartDesc == string &&typeof args.InventoryType == string
-            &&args.InventoryType.length ==3 && args.InventoryType == 'SOI'&&typeof args.Qty == number&&typeof args.SupplierName == string
-            ){
-                return '';
-            }else{
-                return 'check error';
-            }
-            break;
-        }
+        }          
       
       
     }
@@ -99,10 +240,11 @@ var hfc = require('fabric-client');
 }
 
 function CheckDate2(strInputDate) {
-      if (strInputDate == "") return false;
-      strInputDate = strInputDate.replace(/-/g, "/");
-      var d = new Date(strInputDate);
-      if (isNaN(d)) return false;
-      var arr = strInputDate.split("/");
-      return ((parseInt(arr[0], 10) == d.getFullYear()) && (parseInt(arr[1], 10) == (d.getMonth() + 1)) && (parseInt(arr[2], 10) == d.getDate()));
+     var reg = /^(\d{4})(\d{2})(\d{2})$/;
+    var arr = reg.exec(strInputDate);
+    if (strInputDate=="") return true;
+    if (!reg.test(strInputDate)&&RegExp.$2<=12&&RegExp.$3<=31){
+    return false;
+    }
+    return true;
     }
