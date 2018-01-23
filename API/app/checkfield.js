f6c4a1ea8870beaf0aa3974f7866c3aebd0b5627
Kris <andy_liu2012@qq.com>
2018-01-23 16:49:32 +0800

    Update checkfield.js
    修改了checkfield.js文件

--- API/app/checkfield.js
+++ API/app/checkfield.js
@@ -22,8 +22,8 @@ exports.checkField = function (fcn, args) {
             for (var i = 0; i < args.length; i++) {
                 switch (args[i].TRANSDOC) {
                     case 'GR':
-                        if ((typeof args[i].CPONO !== string) || (args[i].CPONO.length > 35)) {
-                            return 'CPONO check constraints are not satisfied.';
+                        if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
+                            return 'FLEXPONO check constraints are not satisfied.';
                         }
                         if (typeof args[i].GRNO !== string) {
                             return 'GRNO check constraints are not satisfied.';
@@ -34,10 +34,13 @@ exports.checkField = function (fcn, args) {
                         if (typeof args[i].GRQTY !== number) {
                             return 'GRQTY check constraints are not satisfied.';
                         }
+                        if ((typeof args[i].GRDate !== string) || (!CheckDate2(args[i].GRDate))) {
+                            return 'GRDate check constraints are not satisfied.';
+                        }
                         break;
 
                     case 'PY':
-                        if ((typeof args[i].CPONO !== string) || (args[i].CPONO.length > 35)) {
+                        if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
                             return 'CPONO check constraints are not satisfied.';
                         }
                         if (args[i].ODMPayments !== '') {
@@ -60,114 +63,99 @@ exports.checkField = function (fcn, args) {
                                 }
                             }
                         }
-                        break;
+                        break; 
+                }
+            }
+            return '';
+        }
 
-                    case 'LP':
-                        if ((typeof args[i].CPONO !== string) && (args[i].CPONO.length > 35)) {
-                            return 'CPONO check constraints are not satisfied.';
-                        }
-                        if (args[i].LOIMaterials !== '') {
-                            for (var j = 0; j < args[i].LOIMaterials.length; j++) {
-                                if (typeof args[i].LOIMaterials[j].RefNo !== string) {
-                                    return 'RefNo check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].LOIMaterials[j].PullType !== string) || (args[i].LOIMaterials[j].PullType !== 'LOI')) {
-                                    return 'PullType check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].LOIMaterials[j].Week !== string) || (args[i].LOIMaterials[j].Week.length !== 8)) {
-                                    return 'Week check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].LOIMaterials[j].NotesToReceiver !== string) || (args[i].LOIMaterials[j].NotesToReceiver.length > 18)) {
-                                    return 'NotesToReceiver check constraints are not satisfied.';
-                                }
-                                if (typeof args[i].LOIMaterials[j].Quantity !== number) {
-                                    return 'Quantity check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].LOIMaterials[j].DlveryDate !== string) || (!CheckDate2(args[i].LOIMaterials[j].DlveryDate))) {
-                                    return 'DlveryDate check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].LOIMaterials[j].RequestedDate !== string) || (!CheckDate2(args[i].LOIMaterials[j].RequestedDate))) {
-                                    return 'RequestedDate check constraints are not satisfied.';
-                                }
-                                if (args[i].LOIMaterials[j].ShipmentInstruction !== string) {
-                                    return 'ShipmentInstruction check constraints are not satisfied.';
-                                }
-                            }
-                        }
+    } else if (fcn === 'crCMaterialPulling') {
+        if (args.length > 0) {
+            for (var i = 0; i < args.length; i++) {
+                if ((typeof args[i].RefNo !== string) || (args[i].length !== 10)) {
+                    return 'RefNo check constraints are not satisfied.'
+                }
+                if ((typeof args[i].PullType !== string) || (args[i].PullType !== 'LOI') || (args[i].PullType !== 'SOI')) {
+                    return 'PullType check constraints are not satisfied.';
+                }
+                if ((typeof args[i].Week !== string) || (args[i].Week.length !== 8)) {
+                    return 'Week check constraints are not satisfied.';
+                }
+                if ((typeof args[i].NotesToReceiver !== string)) {
+                    return 'NotesToReceiver check constraints are not satisfied.';
+                }
+                if ((typeof args[i].Product !== string) || (args[i].Product.length > 18)) {
+                    return 'Product check constraints are not satisfied.';
+                }
+                if (typeof args[i].Quantity !== number) {
+                    return 'Quantity check constraints are not satisfied.'
+                }
+                if ((typeof args[i].DlveryDate !== string) || (!CheckDate2(args[i].DlveryDate))) {
+                    return 'DlveryDate check constraints are not satisfied.';
+                }
+                if ((typeof args[i].RequestedDate !== string) || (!CheckDate2(args[i].RequestedDate))) {
+                    return 'RequestedDate check constraints are not satisfied.';
+                }
+                if (typeof args[i].ShipmentInstruction !== string) {
+                    return 'ShipmentInstruction check constraints are not satisfied.';
+                }
+            }
 
+        }
 
-                        break;
+    } else if (fcn === 'crMappingRefPO') {
+        if (args.length > 0) {
+            for (var i = 0; i < args.length; i++) {
+                if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
+                    return 'FLEXPONO check constraints are not satisfied.';
+                }
+                if (args[i].RefNos.length > 0) {
+                    for (var j = 0; j < args[i].RefNos.length; j++) {
+                        if (typeof args[i].RefNos[j] !== string || args[i].RefNos[j] !== 10) {
+                            return 'RefNo check constraints are not satisfied.';
+                        }
+                    }
+                }
 
-                    case 'LG':
-                        if (typeof args[i].GRNO !== string) {
-                            return 'GRNO check constraints are not satisfied.';
-                        }
-                        if ((typeof args[i].PN !== string) || (args[i].PN.length !== 18)) {
-                            return 'PN check constraints are not satisfied.';
-                        }
-                        if (typeof args[i].Qty !== number) {
-                            return 'Qty check constraints are not satisfied.';
-                        }
-                        if ((typeof args[i].GRDate !== string) || (!CheckDate2(args[i].GRDate))) {
-                            return 'GRDate check constraints are not satisfied.';
-                        }
-                        break;
+            }
+        }
+    } else if (fcn === 'crMappingFlexPO') {
+        if (args.length > 0) {
+            for (var i = 0; i < args.length; i++) {
+                if ((typeof args[i].CPONO !== string) || (args[i].CPONO.length > 35)) {
+                    return 'CPONO check constraints are not satisfied.';
+                }
+                for (var j = 0; j < args[i].FLEXPONO.length; j++) {
+                    if ((typeof args[i].FLEXPONO[j] !== string) || (args[i].FLEXPONO[j].length > 35)) {
+                        return 'FLEXPONO check constraints are not satisfied.';
+                    }
 
-                    case 'SP':
-                        if ((typeof args[i].CPONO !== string) || (args[i].CPONO.length > 35)) {
-                            return 'CPONO check constraints are not satisfied.';
-                        }
-                        if (args[i].SOIPulls !== '') {
-                            for (var j = 0; j < args[i].SOIPulls.length; j++) {
-                                if ((typeof args[i].SOIPulls[j].PullType !== string) || (args[i].SOIPulls[j].PullType !== 'LOI')) {
-                                    return 'PullType check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].SOIPulls[j].Week !== string) || (args[i].SOIPulls[j].Week.length !== 8)) {
-                                    return 'Week check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].SOIPulls[j].PullDate !== string) || (!CheckDate2(args[i].SOIPulls[j].PullDate))) {
-                                    return 'PullDate check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].SOIPulls[j].PN !== string) || (args[i].SOIPulls[j].PN.length > 18)) {
-                                    return 'PN check constraints are not satisfied.';
-                                }
-                                if (typeof args[i].SOIPulls[j].Qty !== number) {
-                                    return 'Qty check constraints are not satisfied.';
-                                }
-                                if ((typeof args[i].SOIPulls[j].RefNo !== string) || (args[i].SOIPulls[j].RefNo.length !== 10)) {
-                                    return 'RefNo check constraints are not satisfied.';
-                                }
-
-                            }
-                        }
-                        break;
-                    case 'SI':
-                        if ((typeof args[i].PN !== string) || (args[i].PN.length > 18)) {
-                            return 'PN check constraints are not satisfied.';
-                        }
-                        if (typeof args[i].PartDesc !== string) {
-                            return 'PartDesc check constraints are not satisfied.';
-                        }
-                        if ((typeof args[i].InventoryType !== string) || (args[i].InventoryType !== 'SOI')) {
-                            return 'InventoryType check constraints are not satisfied.';
-                        }
-                        if (typeof args[i].Qty !== number) {
-                            return 'Qty check constraints are not satisfied.';
-                        }
-                        if (typeof args[i].SupplierName !== string) {
-                            return 'SupplierName check constraints are not satisfied.';
-                        }
-                        break;
-
-                    default:
-                        return '';
                 }
             }
         }
-
+    } else if (fcn === 'crCSOIInventoryInfo') {
+        if (args.length > 0) {
+            for (var i = 0; i < args.length; i++) {
+                if ((typeof args[i].PN !== string) || (args[i].PN.length > 18)) {
+                    return 'PN check constraints are not satisfied.';
+                }
+                if (typeof args[i].PartDesc !== string) {
+                    return 'PartDesc check constraints are not satisfied.';
+                }
+                if ((typeof args[i].InventoryType !== string) || (args[i].InventoryType !== 'SOI')) {
+                    return 'InventoryType check constraints are not satisfied.';
+                }
+                if (typeof args[i].Qty !== number) {
+                    return 'Qty check constraints are not satisfied.';
+                }
+                if (typeof args[i].SupplierName !== string) {
+                    return 'SupplierName check constraints are not satisfied.';
+                }
+            }
+        }
     }
 
-
+    return '';
 }
 
 function CheckDate2(strInputDate) {
