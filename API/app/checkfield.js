0d54336e7633ed3c6618b137417e416888993242
Kris <andy_liu2012@qq.com>
2018-01-24 15:59:26 +0800

    Update checkfield.js
    修改了checkfield.js文件

--- API/app/checkfield.js
+++ API/app/checkfield.js
@@ -16,54 +16,67 @@ var appEnv = cfenv.getAppEnv();
 
 
 exports.checkField = function (fcn, args) {
+
     if (fcn === 'crCPurchaseOrderInfo') {
+
         var reg = /^[a-zA-Z0-9]+$/;
         if (args.length > 0) {
             for (var i = 0; i < args.length; i++) {
+
                 switch (args[i].TRANSDOC) {
                     case 'GR':
-                        if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
+                    logger.debug('liujiang==', args[i].FLEXPONO.length);
+                        if ((typeof (args[i].FLEXPONO) !== "string") || (args[i].FLEXPONO.length > 35)) {
+                            logger.debug('liujiang==', args[i].FLEXPONO.length);
                             return 'FLEXPONO check constraints are not satisfied.';
                         }
-                        if (typeof args[i].GRNO !== string) {
-                            return 'GRNO check constraints are not satisfied.';
-                        }
-                        if ((typeof args[i].PARTNUM.length > 18) || (!reg.test(args[i].PARTNUM))) {
-                            return 'PARTNUM check constraints are not satisfied.';
-                        }
-                        if (typeof args[i].GRQTY !== number) {
-                            return 'GRQTY check constraints are not satisfied.';
-                        }
-                        if ((typeof args[i].GRDate !== string) || (!CheckDate2(args[i].GRDate))) {
-                            return 'GRDate check constraints are not satisfied.';
+                        if (args[i].ODMGRInfos !== '') {
+                            for (var j = 0; j < args[i].ODMGRInfos.length; j++) {
+                                if (typeof args[i].ODMGRInfos[j].GRNO !== "string") {
+                                    logger.debug('liujiang==', typeof args[i].GRNO);
+                                    logger.debug('liujiang==yyyy', args[i].GRNO);
+                                    return 'GRNO check constraints are not satisfied.';
+                                }
+                                logger.debug('liujiang555555==', args[i].ODMGRInfos[j].PARTNUM.length);
+                                if ((typeof args[i].ODMGRInfos[j].PARTNUM.length > 18) || (!reg.test(args[i].ODMGRInfos[j].PARTNUM))) {
+                                    return 'PARTNUM check constraints are not satisfied.';
+                                }
+                                logger.debug('liujiang6666666==', args[i].ODMGRInfos[j].GRQTY.length);
+                                if (typeof args[i].ODMGRInfos[j].GRQTY !== 'number') {
+                                    return 'GRQTY check constraints are not satisfied.';
+                                }
+                                if ((typeof args[i].ODMGRInfos[j].GRDate !== "string") || (!CheckDate2(args[i].ODMGRInfos[j].GRDate))) {
+                                    return 'GRDate check constraints are not satisfied.';
+                                }
+                            }
                         }
                         break;
 
                     case 'PY':
-                        if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
-                            return 'CPONO check constraints are not satisfied.';
+                        if ((typeof args[i].FLEXPONO !== "string") || (args[i].FLEXPONO.length > 35)) {
+                            return 'FLEXPONO check constraints are not satisfied.';
                         }
                         if (args[i].ODMPayments !== '') {
                             for (var j = 0; j < args[i].ODMPayments.length; j++) {
-                                if (typeof args[i].ODMPayments[j].FlexInvoiceNO !== string) {
+                                if (typeof args[i].ODMPayments[j].FlexInvoiceNO !== "string") {
                                     return 'FlexInvoiceNO check constraints are not satisfied.';
                                 }
-                                if (typeof args[i].ODMPayments[j].GRNO !== string) {
+                                if (typeof args[i].ODMPayments[j].GRNO !== "string") {
                                     return 'GRNO check constraints are not satisfied.';
                                 }
-                                if ((typeof args[i].ODMPayments[j].BILLINGNO !== string)
+                                if ((typeof args[i].ODMPayments[j].BILLINGNO !== "string")
                                     || (args[i].ODMPayments[j].BILLINGNO.length !== 10)) {
                                     return 'BILLINGNO check constraints are not satisfied.';
                                 }
-                                if ((typeof args[i].ODMPayments[j].INVOICESTATUS !== string) || (args[i].ODMPayments[j].INVOICESTATUS !== 'A' || args[i].ODMPayments[j].INVOICESTATUS !== 'P')) {
+                                if ((typeof args[i].ODMPayments[j].INVOICESTATUS !== "string") || (args[i].ODMPayments[j].INVOICESTATUS !== 'A' || args[i].ODMPayments[j].INVOICESTATUS !== 'P')) {
                                     return 'INVOICESTATUS check constraints are not satisfied.';
                                 }
-                                if ((typeof args[i].ODMPayments[j].PAYMENTDATE !== string) || (!CheckDate2(args[i].ODMPayments[j].PAYMENTDATE))) {
+                                if ((typeof args[i].ODMPayments[j].PAYMENTDATE !== "string") || (!CheckDate2(args[i].ODMPayments[j].PAYMENTDATE))) {
                                     return 'PAYMENTDATE check constraints are not satisfied.';
                                 }
                             }
                         }
-                        break; 
+                        break;
                 }
             }
             return '';
@@ -72,31 +85,31 @@ exports.checkField = function (fcn, args) {
     } else if (fcn === 'crCMaterialPulling') {
         if (args.length > 0) {
             for (var i = 0; i < args.length; i++) {
-                if ((typeof args[i].RefNo !== string) || (args[i].length !== 10)) {
+                if ((typeof args[i].RefNo !== "string") || (args[i].length !== 10)) {
                     return 'RefNo check constraints are not satisfied.'
                 }
-                if ((typeof args[i].PullType !== string) || (args[i].PullType !== 'LOI') || (args[i].PullType !== 'SOI')) {
+                if ((typeof args[i].PullType !== "string") || (args[i].PullType !== 'LOI') || (args[i].PullType !== 'SOI')) {
                     return 'PullType check constraints are not satisfied.';
                 }
-                if ((typeof args[i].Week !== string) || (args[i].Week.length !== 8)) {
+                if ((typeof args[i].Week !== "string") || (args[i].Week.length !== 8)) {
                     return 'Week check constraints are not satisfied.';
                 }
-                if ((typeof args[i].NotesToReceiver !== string)) {
+                if ((typeof args[i].NotesToReceiver !== "string")) {
                     return 'NotesToReceiver check constraints are not satisfied.';
                 }
-                if ((typeof args[i].Product !== string) || (args[i].Product.length > 18)) {
+                if ((typeof args[i].Product !== "string") || (args[i].Product.length > 18)) {
                     return 'Product check constraints are not satisfied.';
                 }
-                if (typeof args[i].Quantity !== number) {
+                if (typeof args[i].Quantity !== 'number') {
                     return 'Quantity check constraints are not satisfied.'
                 }
-                if ((typeof args[i].DlveryDate !== string) || (!CheckDate2(args[i].DlveryDate))) {
+                if ((typeof args[i].DlveryDate !== "string") || (!CheckDate2(args[i].DlveryDate))) {
                     return 'DlveryDate check constraints are not satisfied.';
                 }
-                if ((typeof args[i].RequestedDate !== string) || (!CheckDate2(args[i].RequestedDate))) {
+                if ((typeof args[i].RequestedDate !== "string") || (!CheckDate2(args[i].RequestedDate))) {
                     return 'RequestedDate check constraints are not satisfied.';
                 }
-                if (typeof args[i].ShipmentInstruction !== string) {
+                if (typeof args[i].ShipmentInstruction !== "string") {
                     return 'ShipmentInstruction check constraints are not satisfied.';
                 }
             }
@@ -106,12 +119,12 @@ exports.checkField = function (fcn, args) {
     } else if (fcn === 'crMappingRefPO') {
         if (args.length > 0) {
             for (var i = 0; i < args.length; i++) {
-                if ((typeof args[i].FLEXPONO !== string) || (args[i].FLEXPONO.length > 35)) {
+                if ((typeof args[i].FLEXPONO !== "string") || (args[i].FLEXPONO.length > 35)) {
                     return 'FLEXPONO check constraints are not satisfied.';
                 }
                 if (args[i].RefNos.length > 0) {
                     for (var j = 0; j < args[i].RefNos.length; j++) {
-                        if (typeof args[i].RefNos[j] !== string || args[i].RefNos[j] !== 10) {
+                        if (typeof args[i].RefNos[j] !== "string" || args[i].RefNos[j] !== 10) {
                             return 'RefNo check constraints are not satisfied.';
                         }
                     }
@@ -122,11 +135,11 @@ exports.checkField = function (fcn, args) {
     } else if (fcn === 'crMappingFlexPO') {
         if (args.length > 0) {
             for (var i = 0; i < args.length; i++) {
-                if ((typeof args[i].CPONO !== string) || (args[i].CPONO.length > 35)) {
+                if ((typeof args[i].CPONO !== "string") || (args[i].CPONO.length > 35)) {
                     return 'CPONO check constraints are not satisfied.';
                 }
                 for (var j = 0; j < args[i].FLEXPONO.length; j++) {
-                    if ((typeof args[i].FLEXPONO[j] !== string) || (args[i].FLEXPONO[j].length > 35)) {
+                    if ((typeof args[i].FLEXPONO[j] !== "string") || (args[i].FLEXPONO[j].length > 35)) {
                         return 'FLEXPONO check constraints are not satisfied.';
                     }
 
@@ -136,19 +149,19 @@ exports.checkField = function (fcn, args) {
     } else if (fcn === 'crCSOIInventoryInfo') {
         if (args.length > 0) {
             for (var i = 0; i < args.length; i++) {
-                if ((typeof args[i].PN !== string) || (args[i].PN.length > 18)) {
+                if ((typeof args[i].PN !== "string") || (args[i].PN.length > 18)) {
                     return 'PN check constraints are not satisfied.';
                 }
-                if (typeof args[i].PartDesc !== string) {
+                if (typeof args[i].PartDesc !== "string") {
                     return 'PartDesc check constraints are not satisfied.';
                 }
-                if ((typeof args[i].InventoryType !== string) || (args[i].InventoryType !== 'SOI')) {
+                if ((typeof args[i].InventoryType !== "string") || (args[i].InventoryType !== 'SOI')) {
                     return 'InventoryType check constraints are not satisfied.';
                 }
-                if (typeof args[i].Qty !== number) {
+                if (typeof args[i].Qty !== 'number') {
                     return 'Qty check constraints are not satisfied.';
                 }
-                if (typeof args[i].SupplierName !== string) {
+                if (typeof args[i].SupplierName !== "string") {
                     return 'SupplierName check constraints are not satisfied.';
                 }
             }
@@ -159,12 +172,14 @@ exports.checkField = function (fcn, args) {
 }
 
 function CheckDate2(strInputDate) {
-    var parts;
+    var parts = [];
     if (strInputDate.length === 8) {
         parts[0] = strInputDate.substring(0, 4);
         parts[1] = strInputDate.substring(4, 6);
         parts[2] = strInputDate.substring(6, 8);
-
+        logger.debug('liujiang==', parts[0]);
+        logger.debug('liujiang==', parts[1]);
+        logger.debug('liujiang==', parts[2]);
         for (var i = 0; i < 3; i++) {
             //如果构成日期的某个部分不是数字，则返回false
             if (isNaN(parts[i])) {
@@ -177,40 +192,44 @@ function CheckDate2(strInputDate) {
         if (y > 3000) {
             return false;
         }
-        var smonth = "01,02,03,04,05,06,07,08,09,10,11,12";
+        var smonth = '01,02,03,04,05,06,07,08,09,10,11,12';
         if (smonth.indexOf(m) == -1) {
             return false;
         }
-        var sday = "01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31";
+        var sday = '01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31';
         if (sday.indexOf(d) == -1) {
             return false;
         }
         switch (d) {
-            case 29:
-                if (m == "02") {
+            case '29':
+                if (m == '02') {
                     //如果是2月份
-                    if (((y % 4) == 0) && ((y % 100) != 0) || ((y % 400) == 0)) {
+                    if (((y % 4) === 0) && ((y % 100) !== 0) || ((y % 400) === 0)) {
                         //如果年份能被100整除但不能被400整除 (即闰年)
                     } else {
                         return false;
                     }
                 }
                 break;
-            case 30:
-                if (m == "02") {
+            case '30':
+                if (m === '02') {
                     //2月没有30日
                     return false;
                 }
                 break;
-            case 31:
-                if (m == "02" || m == "04" || m == "06" || m == "09" || m == "11") {
+            case '31':
+                if (m === '02' || m === '04' || m === '06' || m === '09' || m === '11') {
                     //2、4、6、9、11月没有31日
+                    logger.debug('liujiang==', '2018201820182018');
                     return false;
                 }
                 break;
             default:
+                logger.debug('liujiang==', '299999999999999999999999999');
+                return true;
+
         }
-        return true;
+
     } else {
         return false;
     }
